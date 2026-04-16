a67c5f19fe38b0933bf6ac47a9d5138c5106c7b3
Parth-Pingle10 <onlynonlyminecraft@gmail.com>
2026-04-16 13:53:23 +0530

    Added mongoDB for storing url and other metadata

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 import httpx
 import asyncio
-
-app = FastAPI()
+from mongo import client,db,collection
+from datetime import datetime,timezone
+from contextlib import asynccontextmanager
 
 active_task = {}
 
@@ -11,15 +12,53 @@ async def ping_website(url:str):
         while True:
             try:
                 res = await client.get(url)
+                status = "active" if res.status_code == 200 else "failed"
+                if status == "active":
+                    await collection.update_one({"url":url},
+                                            {
+                                                "$inc" : {"ping_count":1},
+                                                "$set" : {"last_ping":datetime.now(timezone.utc),
+                                                         "status":status}
+                                            })
+                
+                else:
+                    await collection.update_one({"url":url},
+                                            {
+                                               
+                                                "$set" : {"last_ping": datetime.now(timezone.utc),
+                                                         "status":status}
+                                            })
                 print(f"{url} -> {res.status_code}")
+                await asyncio.sleep(180)
+                
+                
             except asyncio.CancelledError:
-                print(f"Stopped pinging {url}")
+                await collection.update_one({"url":url},{
+                                            "$set":{"last_ping": datetime.now(timezone.utc),"status":"stopped"}
+                                            })
                 break
             except Exception as e:
+                await collection.update_one({"url":url},{
+                                            "$set":{"last_ping": datetime.now(timezone.utc),"status":"failed"}
+                                            })
                 print(f"{url} -> {e}")
                 
-            await asyncio.sleep(500)
-            
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    async for doc in collection.find({"status":"active"}):
+        url = doc["url"]
+        task = asyncio.create_task(ping_website(url))
+        active_task[url]=task
+    
+    yield
+    
+    for task in active_task.values():
+        task.cancel()
+    
+    active_task.clear()
+    
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
@@ -30,12 +69,28 @@ async def root():
 async def start(url:str):
     if not url.startswith("http"):
         url = "https://"+url
+        
+    existing = await collection.find_one({"url":url})
+    
+    if existing and existing["status"] == "active" and url in active_task:
+        return {"message": f"Already running {url}"}
     
     if url in active_task:
-        return {"message": f"Already running {url}"}    
+        active_task[url].cancel()
+        del active_task[url]
+    
+    if not existing:
+        await collection.insert_one({
+            "url":url,
+            "ping_count":0,
+            "last_ping":None,
+            "status":"active",
+            "created_at":datetime.now(timezone.utc),
+        }) 
+    else:
+        await collection.update_one({"url":url},{"$set":{"status":"active"}})
     
     task = asyncio.create_task(ping_website(url))
-    
     active_task[url] = task
     
     return{"message":f"Started pinging {url}"}
@@ -46,13 +101,19 @@ async def stop(url:str):
     if not url.startswith("http"):
         url = "https://"+url
     
-    if url not in active_task:
+    existing = await collection.find_one({"url":url})
+
+    if not existing or existing.get("status") != "active":
         return {"message": f"{url} is not running"}
-        
-    task = active_task.get(url)
     
+    task = active_task.get(url)
     if task:
         task.cancel()
         del active_task[url]
+        
+    await collection.update_one(
+    {"url": url},
+    {"$set": {"status": "stopped"}}
+    )
         
     return {"message":f"Stopped pinging {url}"}
